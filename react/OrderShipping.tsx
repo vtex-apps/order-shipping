4fd4f089ab8f122dc5313a9b9f33604b53a9b528
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-04-01 12:05:19 -0300

    upgrade packages and fix lint issues

--- react/OrderShipping.tsx
+++ react/OrderShipping.tsx
@@ -1,14 +1,12 @@
-import React, { createContext, ReactNode, useContext, useCallback } from 'react'
-import { compose, graphql } from 'react-apollo'
-import {
-  QueueStatus,
-  useOrderQueue,
-  useQueueStatus,
-} from 'vtex.order-manager/OrderQueue'
-import { useOrderForm } from 'vtex.order-manager/OrderForm'
+import React, { createContext, useContext, useCallback } from 'react'
+import { useMutation } from 'react-apollo'
+import { OrderQueue, OrderForm } from 'vtex.order-manager'
+import EstimateShippingMutation from 'vtex.checkout-resources/MutationEstimateShipping'
+import SelectDeliveryOptionMutation from 'vtex.checkout-resources/MutationSelectDeliveryOption'
 
-import EstimateShipping from 'vtex.checkout-resources/MutationEstimateShipping'
-import SelectDeliveryOption from 'vtex.checkout-resources/MutationSelectDeliveryOption'
+const { QueueStatus, useOrderQueue, useQueueStatus } = OrderQueue
+
+const { useOrderForm } = OrderForm
 
 interface InsertAddressResult {
   success: boolean
@@ -21,12 +19,6 @@ interface Context {
   insertAddress: (address: CheckoutAddress) => Promise<InsertAddressResult>
   deliveryOptions: DeliveryOption[]
   selectDeliveryOption: (option: string) => void
-}
-
-interface OrderShippingProps {
-  children: ReactNode
-  EstimateShipping: any
-  SelectDeliveryOption: any
 }
 
 const OrderShippingContext = createContext<Context | undefined>(undefined)
@@ -73,124 +65,111 @@ const findDeliveryOptionById = (
   })!
 }
 
-export const OrderShippingProvider = compose(
-  graphql(EstimateShipping, { name: 'EstimateShipping' }),
-  graphql(SelectDeliveryOption, { name: 'SelectDeliveryOption' })
-)(
-  ({
-    children,
-    EstimateShipping,
-    SelectDeliveryOption,
-  }: OrderShippingProps) => {
-    const { enqueue, listen } = useOrderQueue()
-    const { orderForm, setOrderForm } = useOrderForm()
+export const OrderShippingProvider: React.FC = ({ children }) => {
+  const [EstimateShipping] = useMutation(EstimateShippingMutation)
+  const [SelectDeliveryOption] = useMutation(SelectDeliveryOptionMutation)
 
-    const queueStatusRef = useQueueStatus(listen)
+  const { enqueue, listen } = useOrderQueue()
+  const { orderForm, setOrderForm } = useOrderForm()
 
-    const {
-      canEditData,
-      shipping: { countries, selectedAddress, deliveryOptions },
-    } = orderForm
+  const queueStatusRef = useQueueStatus(listen)
 
-    const insertAddress = useCallback(
-      async (address: CheckoutAddress) => {
-        const task = async () => {
-          const {
-            data: { estimateShipping: newOrderForm },
-          } = await EstimateShipping({
-            variables: {
-              addressInput: address,
-            },
-          })
+  const {
+    canEditData,
+    shipping: { countries, selectedAddress, deliveryOptions },
+  } = orderForm
 
-          return newOrderForm
-        }
-
-        try {
-          const newOrderForm = await enqueue(task, 'insertAddress')
-
-          if (queueStatusRef.current === QueueStatus.FULFILLED) {
-            setOrderForm(newOrderForm)
-          }
-
-          return { success: true }
-        } catch (error) {
-          if (!error || error.code !== TASK_CANCELLED) {
-            throw error
-          }
-          return { success: false }
-        }
-      },
-      [EstimateShipping, enqueue, queueStatusRef, setOrderForm]
-    )
-
-    const selectDeliveryOption = useCallback(
-      (deliveryOptionId: string) => {
-        const { price } = findDeliveryOptionById(
-          deliveryOptions,
-          deliveryOptionId
-        )
-
-        const shipping = getShipping(orderForm.totalizers)
-        if (!shipping) {
-          throw new Error('Shipping totalizer not found')
-        }
-
-        const oldShippingPrice = shipping.value
-
-        const newOrderForm = {
-          ...orderForm,
-          shipping: {
-            ...orderForm.shipping,
-            deliveryOptions: changeSelectedDeliveryOption(
-              deliveryOptions,
-              deliveryOptionId
-            ),
+  const insertAddress = useCallback(
+    async (address: CheckoutAddress) => {
+      const task = async () => {
+        const {
+          data: { estimateShipping: newOrderForm },
+        } = await EstimateShipping({
+          variables: {
+            addressInput: address,
           },
-          totalizers: updateShipping(orderForm.totalizers, price),
-          value: orderForm.value - oldShippingPrice + price,
-        }
-        setOrderForm(newOrderForm)
-        const task = async () => {
-          const {
-            data: { selectDeliveryOption: newOrderForm },
-          } = await SelectDeliveryOption({
-            variables: {
-              deliveryOptionId: deliveryOptionId,
-            },
-          })
+        })
 
-          return newOrderForm
+        return newOrderForm
+      }
+
+      try {
+        const newOrderForm = await enqueue(task, 'insertAddress')
+
+        if (queueStatusRef.current === QueueStatus.FULFILLED) {
+          setOrderForm(newOrderForm)
         }
 
-        enqueue(task, 'selectDeliveryOption')
-      },
-      [
-        SelectDeliveryOption,
+        return { success: true }
+      } catch (error) {
+        if (!error || error.code !== TASK_CANCELLED) {
+          throw error
+        }
+        return { success: false }
+      }
+    },
+    [EstimateShipping, enqueue, queueStatusRef, setOrderForm]
+  )
+
+  const selectDeliveryOption = useCallback(
+    (deliveryOptionId: string) => {
+      const { price } = findDeliveryOptionById(
         deliveryOptions,
-        enqueue,
-        queueStatusRef,
-        orderForm,
-        setOrderForm,
-      ]
-    )
+        deliveryOptionId
+      )
 
-    return (
-      <OrderShippingContext.Provider
-        value={{
-          canEditData,
-          countries,
-          selectedAddress,
-          insertAddress,
-          deliveryOptions,
-          selectDeliveryOption,
-        }}
-      >
-        {children}
-      </OrderShippingContext.Provider>
-    )
-  }
-)
+      const shipping = getShipping(orderForm.totalizers)
+      if (!shipping) {
+        throw new Error('Shipping totalizer not found')
+      }
+
+      const oldShippingPrice = shipping.value
+
+      const newOrderForm = {
+        ...orderForm,
+        shipping: {
+          ...orderForm.shipping,
+          deliveryOptions: changeSelectedDeliveryOption(
+            deliveryOptions,
+            deliveryOptionId
+          ),
+        },
+        totalizers: updateShipping(orderForm.totalizers, price),
+        value: orderForm.value - oldShippingPrice + price,
+      }
+      setOrderForm(newOrderForm)
+      const task = async () => {
+        const {
+          data: { selectDeliveryOption: updatedOrderForm },
+        } = await SelectDeliveryOption({
+          variables: {
+            deliveryOptionId,
+          },
+        })
+
+        return updatedOrderForm
+      }
+
+      enqueue(task, 'selectDeliveryOption')
+    },
+    [SelectDeliveryOption, deliveryOptions, enqueue, orderForm, setOrderForm]
+  )
+
+  return (
+    <OrderShippingContext.Provider
+      value={{
+        canEditData,
+        countries,
+        selectedAddress,
+        insertAddress,
+        deliveryOptions,
+        selectDeliveryOption,
+      }}
+    >
+      {children}
+    </OrderShippingContext.Provider>
+  )
+}
 
 export const useOrderShipping = () => {
   const context = useContext(OrderShippingContext)
---
+++ react/typings/vtex.checkout-resources.d.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/export */
+
+declare module 'vtex.checkout-resources/MutationEstimateShipping' {
+  export { default } from 'vtex.checkout-resources/react/MutationEstimateShipping'
+}
+
+declare module 'vtex.checkout-resources/MutationSelectDeliveryOption' {
+  export { default } from 'vtex.checkout-resources/react/MutationSelectDeliveryOption'
+}
