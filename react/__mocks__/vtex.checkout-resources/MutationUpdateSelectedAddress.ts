c11a41e76faca27de366eee14ed67dbd26e522dc
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-04-22 15:17:14 -0300

    fix typo

--- react/__mocks__/vtex.checkout-resources/MutationUpdateSelectedAddress.ts
+++ react/__mocks__/vtex.checkout-resources/MutationUpdateSelectedAddress.ts
@@ -2,7 +2,7 @@ import gql from 'graphql-tag'
 
 export default gql`
   mutation MockUpdateSelectedAddress($address: AddressInput!) {
-    updateSelectedAddresS(input: $address) {
+    updateSelectedAddress(input: $address) {
       shipping {
         selectedAddress {
           id
